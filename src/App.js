ee94f0d7adb181f3303786af54414759df656b5f
unknown <vicentasysyamsbioma@gmail.com>
2022-04-17 09:39:18 +0700

    web1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Router, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {Route, Router, Routes} from 'react-router-dom';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Navbar</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Navbar;
